7ff6516d3e1a3b6276d452c659f265a4e919ce9f
jsjzh <kimimi_king@163.com>
2020-05-25 10:39:08 +0800

    feat: 更改 ts 的打包结果

--- packages/typescript/src/example/random/index.js
+++ packages/typescript/src/example/random/index.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const createRandomCount = (limit) => Math.round(Math.random() * limit);
+const preNumber = (num) => (num < 10 ? `0${num}` : String(num));
 const wrapMainFun = (len, count) => {
     const arr = [];
     while (arr.length < len) {
@@ -10,10 +11,4 @@ const wrapMainFun = (len, count) => {
     }
     return arr;
 };
-const fixCount = (num) => (num < 10 ? `0${num}` : String(num));
-exports.default = ([arr1Len, arr1Count] = [5, 35], [arr2Len, arr2Count] = [2, 12]) => {
-    return [
-        ...wrapMainFun(arr1Len, arr1Count),
-        ...wrapMainFun(arr2Len, arr2Count),
-    ].map(fixCount);
-};
+exports.default = ([arr1Len, arr1Count] = [5, 35], [arr2Len, arr2Count] = [2, 12]) => [...wrapMainFun(arr1Len, arr1Count), ...wrapMainFun(arr2Len, arr2Count)].map(preNumber);
--- packages/typescript/typings/example/random/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare type IArr = [number, number];
-declare const _default: ([arr1Len, arr1Count]?: IArr, [arr2Len, arr2Count]?: IArr) => string[];
-export default _default;
